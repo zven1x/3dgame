289fc007717d2831b8c3142055d68baffa58cec2
zven1x <51484893+zven1x@users.noreply.github.com>
2019-06-19 18:07:05 +0200

    creating comments for health script

--- Health.cs
+++ Health.cs
@@ -8,15 +8,17 @@ public class Health : MonoBehaviour
 {
 
     [SerializeField]
-
+    // tworzenie zmiennej dla parametra zdrowia.
     private int startingHealth = 5;
 
 
-
+    // tworzenie zmiennej dla obecnego parametru zdrowia.
     private int currentHealth;
 
 
-
+    /// <summary>
+    /// meoda jest stworzona dla przenoszenia zdrowia startowego do zdrowia obecngo.
+    /// </summary>
     private void OnEnable()
 
     {
@@ -26,7 +28,11 @@ public class Health : MonoBehaviour
     }
 
 
-
+    /// <summary>
+    /// metoda jest tworzona dla spowodowania i pobierania szkody.
+    /// w przypadku kiedy obecne zdrowie jest mniejsze lub rowne od 0 wywoluje sie metoda Die.
+    /// </summary>
+    /// <param name="damageAmount"></param>
     public void TakeDamage(int damageAmount)
 
     {
@@ -42,7 +48,10 @@ public class Health : MonoBehaviour
     }
 
 
-
+    /// <summary>
+    /// metoda jest wywolana w TakeDamage w tym przypadku kiedy poziom zdrowia jest mniejszy lub rowny 0 
+    /// wykonuje SetActiwe(false) czyli deaktywuje object.
+    /// </summary>
     private void Die()
 
     {
