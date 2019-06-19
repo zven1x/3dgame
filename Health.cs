46df97b3fe1556f1c55c55967ccdd4f7c86f1e63
zven1x <51484893+zven1x@users.noreply.github.com>
2019-06-19 18:05:49 +0200

    creating script for health

---
+++ Health.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Health : MonoBehaviour
+
+{
+
+    [SerializeField]
+
+    private int startingHealth = 5;
+
+
+
+    private int currentHealth;
+
+
+
+    private void OnEnable()
+
+    {
+
+        currentHealth = startingHealth;
+
+    }
+
+
+
+    public void TakeDamage(int damageAmount)
+
+    {
+
+        currentHealth -= damageAmount;
+
+
+
+        if (currentHealth <= 0)
+
+            Die();
+
+    }
+
+
+
+    private void Die()
+
+    {
+
+        gameObject.SetActive(false);
+
+    }
+
+}
