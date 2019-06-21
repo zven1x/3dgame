b53537f18e9c9cd6b9a6d6aca78d35950651d8c8
zven1x <51484893+zven1x@users.noreply.github.com>
2019-06-21 18:00:26 +0200

    Creating comments for script ScoreScript

--- ScoreScript.cs
+++ ScoreScript.cs
@@ -10,14 +10,19 @@ public class ScoreScript : MonoBehaviour
     internal static string text;
     Text score;
 
-
+    /// <summary>
+    /// Metoda wykonuje pobieranie terazniejszego poziomu Score.
+    /// ktora wyswietla go w score.text.
+    /// </summary>
     private void Start()
     {
         score = GetComponent<Text>();
     }
 
 
-
+    /// <summary>
+    /// dodajemy do metody Update funkcje ktora wykonuje dodawanie terazniejszego poziomu score do tego ktory otrzymuje z gry w scoreValue.
+    /// </summary>
     void Update()
     {
         score.text = "Score:" + scoreValue;
