4e6609681e46e3efcf35cbea674d328bed4d3b72
zven1x <51484893+zven1x@users.noreply.github.com>
2019-06-21 17:44:50 +0200

    Creating comments

--- EnemyMovement.cs
+++ EnemyMovement.cs
@@ -5,13 +5,17 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+
     private NavMeshAgent navMeshAgent;
     private Animator animator;
     private AggroDetection aggroDetection;
 
     private Transform target;
    
-
+    /// <summary>
+    /// dodajemy do metody Awake pola ktore bedzie wlaczac animacje i uruchomienie persony.
+    /// metoda wykouje sie przy podejsciu glownej persony do enemy.
+    /// </summary>
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -20,11 +24,18 @@ public class EnemyMovement : MonoBehaviour
         aggroDetection.OnAggro += AggroDetection_OnAggro;
    
      }
-
+    /// <summary>
+    /// metoda wywoluje sie przy podejsciu glownej persony do enemy.
+    /// metowa wykonuje przesylanie zmiennej target jaka Action.
+    /// </summary>
+    /// <param name="target"></param>
     private void AggroDetection_OnAggro(Transform target)
     {
         this.target = target;
     }
+    /// <summary>
+    /// dodajmy do metody Update pola ktore tworzy dystancje do glownej persony od enemy .
+    /// </summary>
     private void Update()
     {
         if (target != null)
