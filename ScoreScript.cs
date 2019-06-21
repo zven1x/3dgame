831558335b1341a41321ad1f716a0cd5b35e7b68
zven1x <51484893+zven1x@users.noreply.github.com>
2019-06-21 17:29:29 +0200

    creating script for score in game

---
+++ ScoreScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    public static int scoreValue = 0;
+    internal static string text;
+    Text score;
+
+
+    private void Start()
+    {
+        score = GetComponent<Text>();
+    }
+
+
+
+    void Update()
+    {
+        score.text = "Score:" + scoreValue;
+        
+    }
+
+}
