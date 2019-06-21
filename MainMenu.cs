6fef598eb50f703c6d64fb5f80998afcf7087df2
zven1x <51484893+zven1x@users.noreply.github.com>
2019-06-21 19:40:46 +0200

    Creating script for MainMenu

---
+++ MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour{
+/// <summary>
+/// metoda wykonuje zmine sceny 'menu' na scenu 'Game' po kliknieciu 'button play'.
+/// </summary>
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    /// <summary>
+    /// metoda wykonuje zamkniecie gry po kliknieciu 'button quit'.
+    /// </summary>
+    public void QuitGame()
+    {
+
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+}
