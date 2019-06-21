e2b1b10b0f6d110834dcc0a34e80f803cc42cbdb
zven1x <51484893+zven1x@users.noreply.github.com>
2019-06-21 17:27:22 +0200

    creating scripts for enemy attack

---
+++ AggroDetecion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AggroDetection : MonoBehaviour
+{
+    public event Action<Transform> OnAggro = delegate { };
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<PlayerMovements>();
+        if(player!=null)
+        {
+            OnAggro(player.transform);
+            Debug.Log("Aggrod");
+           
+        }
+
+    }
+
+}
---
+++ Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{ 
+    private AggroDetection aggroDetection;
+    private Health healthTarget;
+    private float attackRefreshRate = 1;
+    private int attackTimer;
+
+    private void Awake()
+    {
+        aggroDetection = GetComponent<AggroDetection>();
+        aggroDetection.OnAggro += AggroDetection_OnAggro;
+    }
+    private void AggroDetection_OnAggro(Transform target)
+    {
+
+        Health health = target.GetComponent<Health>();
+        if (health != null)
+        {
+            healthTarget = health;
+        }
+    }
+    private void Update()
+    {
+        if (healthTarget !=null)
+        {
+            if (CanAttack())
+            {
+                Attack();
+            }
+        }
+    }
+
+    private bool CanAttack()
+    {
+        return attackTimer >= attackRefreshRate;
+    }
+
+    private void Attack()
+    {
+        throw new NotImplementedException();
+    }
+}
